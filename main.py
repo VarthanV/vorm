5d2212732eff5b34c099c0e420c1f76db14dfbda
Vishnu Varthan <vishnulatha006@gmail.com>
2021-07-11 15:42:18 +0530

    Complete foreign key querying 🔥

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from os import PRIO_PGRP
 from pprint import pprint
-from vorm.base import *
+import vorm
 from vorm.manager import ConnectionManager
+from vorm.base import *
 from vorm import fields
 
 db_settings = {
@@ -16,9 +17,6 @@ db_settings = {
 ConnectionManager.create_connection(db_settings)
 
 
-
-
-
 class Student(BaseModel):
     table_name = "students"
     name = fields.CharField(max_length=250, nullable=False, default="Johny")
@@ -28,18 +26,26 @@ class Student(BaseModel):
 
 
 class Klass(BaseModel):
-    table_name="class"
-    name = fields.CharField(max_length=250,nullable=True)
+    table_name = "class"
+    name = fields.CharField(max_length=250, nullable=True)
     student = fields.ForeignKey(Student)
 
 
-#ConnectionManager.migrate(Student)
-#ConnectionManager.migrate(Student)
-#ConnectionManager.migrate(Klass)
+# ConnectionManager.migrate(Student)
+# ConnectionManager.migrate(Student)
+# ConnectionManager.migrate(Klass)
 # ConnectionManager.migrate(Employee)
 # students = Student.objects.where(name__eq='vishnu',salary__gte=2000)
 # for i in students :
 #     print(i.name)
 
-s = Student.objects.get_one(name__eq='vishnu',salary__eq=50000)
-print(s.name)
+# s = Student.objects.get_one(name__eq='vishnu',salary__eq=50000)
+# print(s.name)
+
+# k = Klass.objects.get_one(id__eq=1)
+# print(k)
+
+k = Klass.objects.where(name__eq='A', fetch_relations=True)
+s = Student.objects.get_one(id__eq=1)
+
+klass_student_has_enrolled = Klass.objects.where(student_id__eq=s.id)
