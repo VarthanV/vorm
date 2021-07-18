85a13b913f6bb6ca1eca5585352f7bdbab61af2b
Vishnu Varthan <vishnulatha006@gmail.com>
2021-07-18 16:11:12 +0530

    Updated setup file

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from distutils.core import setup
-from setuptools import PackageFinder, setup, setuptools, version
+import setuptools
 import os
 
 
@@ -8,12 +8,12 @@ README_PATH = os.path.join(os.path.dirname(__file__), 'README.md')
 with open(README_PATH) as readme_file:
     readme = readme_file.read()
 
-os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+#os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='vorm',
-    version='0.0.2',
-    package=setuptools.find_packages(),
+    version='1.0.0',
+    packages=setuptools.find_packages(),
     include_packages_data=True,
     long_description_content_type='text/markdown',
     long_description=open('README.md').read(),
@@ -21,15 +21,7 @@ setup(
     author="Vishnu Varthan",
     author_email="vishnulatha006@gmail.com",
     url="https://github.com/VarthanV/vorm",
-    install_requires=[
-        "autopep8==1.5.7",
-        "mysql-connector-python==8.0.25",
-        "protobuf==3.17.3",
-        "pycodestyle==2.7.0",
-        "six==1.16.0",
-        "toml==0.10.2",
-
-    ],
+    install_requires=[],
     license="MIT License",
     zip_safe=False,
     keywords="orm,flask,django",
@@ -41,5 +33,5 @@ setup(
         'Topic :: Software Development :: Libraries',
         'Topic :: Utilities',
     ],
-
+    py_modules = ["vorm"],   
 )
